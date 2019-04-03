5e5634758beb24eabf8652a94c245c1464bd863c
Phoboe <contactatnina@gmail.com>
2019-04-03 11:53:18 +0200

    Maj nina

--- classes/locals/contenu.py
+++ classes/locals/contenu.py
@@ -1,43 +1,53 @@
-lines = [
-"   ",
-"   ",
-"   ",
-"   Bienvenue dans le loup-garou !",
-"   Voici les règles :",
-"   ",
-"   Le but du jeu :",
-"   Pour les villageaois : éliminer les loups-garous",
-"   pour les loups-garou : éliminer les villageois",
-"   ",
-"   Comment fonctionne le jeu :",
-"   ",
-"   1 - Mise en place :",
-"       Les joueurs désignent ou tirent au sort un meneur de jeu qui ne joue pas, mais dirige la partie. Pour les premières parties, quelqu'un qui connaît bien le jeuou celui qui a déchiffré la règle ou un habile meneur de groupe sera le plus à même de restituer la meilleure ambiance. Le meneur de jeu distribue à chaque joueur 1 carte personnage face cachée. Chaque joueur regarde discrètement son personnage, puis repose sa carte face cachée devant lui.",
-"""   Le meneur endort le village. Le meneur dit: "C'est la nuit tout le village s'endort, les joueurs ferment les yeux". Tous les joueurs baissent la tête et ferment les yeux. Puis selon le choix des personnages en jeu, le meneur appelle les différents personnages.""",
-"   ",
-"   2 - Tour de préparation :",
-"""     - Le meneur appelle le Voleur:
-Le meneur dit: "le Voleur se réveille !". Le joueur qui possède la carte Voleur, ouvre les yeux et regarde discrètement les deux cartes cachées au milieu, puis change éventuellement de personnage.
-Le meneur dit : "Le Voleur se rendort". Le Voleur referme les yeux.
 
-- Le meneur appelle Cupidon
-Le meneur dit: "Cupidon se réveille !". Cupidon ouvre les yeux et désigne deux joueurs (dont éventuellement lui-même). Le meneur fait le tour de la table et touche discrètement le dos des deux Amoureux.
-Le meneur dit: "Cupidon se rendort". Cupidon referme les yeux.
 
-- Le meneur appelle les Amoureux
-Le meneur dit: "les Amoureux se réveillent, se reconnaissent, et se rendorment !". Ils ne se montrent pas leur carte de sorte que chacun ignore la véritable personnalité de l'être aimé. Puis le meneur suit le tour normal.""",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   ",
-"   "
 
-        ]
+telling = [
+"La nuit tombe.",
+"Le jour se lève.",
+
+"Les loups-garous se réveillent.",
+"Les villageois se réveillent.",
+"la sorcière se réveille.",
+"Cupidon se réveille.",
+"La voyante se réveille.",
+"Le voleur se réveille.",
+"Le chasseur se réveille.",
+"La petite fille se réveille.",
+"Le capitaine se réveille.",
+
+"Les loups-garous se rendorment.",
+"Les villageois se rendorment.",
+"la sorcière se rendort.",
+"Cupidon se rendort.",
+"La voyante se rendort.",
+"Le voleur se rendort.",
+"Le chasseur se rendort.",
+"La petite fille se rendort.",
+"Le capitaine se rendort.",
+
+"Loups-garous ; choisissez un villageois à attaquer cette nuit.",
+"Villageois, quel loup-garou voulez-vous tuer aujourd'hui ?",
+"Sorcière, laquelle de tes potions veux-tu utiliser ? (réssuciter -ou- tuer)",
+"Cupidon, quelles personnes souhaites tu lier par les liens de l'amour ?",
+"Voyante, quelle carte voulez-vous voir ?",
+"Voleur, quelle carte souhaites tu ?",
+"Chasseur, tu es mort, quel joueur veux-tu tuer ?",
+"Petite-fille, voila ce qu'il se passe.",
+"Capitaine, n'oublies pas que tes votes valent 2 voix.",
+"Capitaine, quel seccesseur désignes-tu ?",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"les loups garous ; choisissez une proie pour cette nuit.",
+
+
+
+]
